7e3be95e60e27d80d67703ec458d39a5a88cbb7f
Ayush Sharma <ayushanavsharma2@gmail.com>
2026-02-18 15:59:28 +1200

    Phase 1: Added file check to see if it has been pointed correctly to and that it exists.

--- src/process_file.py
+++ src/process_file.py
@@ -1,9 +1,19 @@
 import sys 
+import os
 
 def get_file_arg():
     try:
-        sys.argv[1]
+        if(len(sys.argv[1]) < 1):
+            raise Exception("check to see if you have supplied the file")
         
+        if(os.path.exists(f'{os.path.abspath(sys.argv[1])}.pdf')):
+            print("file exists")
+            # call open_file()
+        else:
+            raise Exception("file not found :(")
+            
     except Exception as e:
         print("Check to see if you have passed the file name!")
-        print("open run.sh to see what to do :)")
+        print("open run.sh to see what to do :)\n")
+        print("--------detailed error--------")
+        print(e)
