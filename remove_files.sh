29e84bab5f74f9442cb56a731aab4499939ababf
Ayush Sharma <ayushanavsharma2@gmail.com>
2026-02-19 18:23:26 +1200

    notifies user what files have been deleted

--- remove_files.sh
+++ remove_files.sh
@@ -1,8 +1,10 @@
 # follow syntax properly, thats how it is written with the right spaces
 if [ -f lowest_to_highest.txt ]; then 
 	rm lowest_to_highest.txt
+	echo lowest_to_highest.txt deleted
 elif [ -f highest_to_lowest.txt ]; then
 	rm highest_to_lowest.txt
+	echo highest_to_lowest.txt deleted
 else
 	echo nothing to delete
 fi
